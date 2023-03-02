559b18ed17fdfab18ec8497e229939f39e8383da
guocailee <guocailee@gmail.com>
2023-03-02 14:31:33 +0800

    project init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hixtrip Online Quiz
+
+## 准备
+
+- fork 此仓库
+- 根据如下要求实现相关代码
+- 完成要求
+  - 提交`pull request`
+  - 提供页面截图
+
+## 需求
+1. 目前已经有`src/api/user.ts` 以及 `src/api/org.ts` 两个API
+2. 需要实现如下功能：
+
+[](./docs/preview.jpg)
+
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import userApi from './api/user'
+import orgApi from './api/org'
+
+let user = ref<any>()
+let org = ref<any>()
+onMounted(() => {
+  userApi.query({}).then((res) => (user.value = res))
+  orgApi.query().then((res) => (org.value = res))
+})
+</script>
+
+<template>
+  <div>
+    {{ org }}
+    {{ user }}
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
